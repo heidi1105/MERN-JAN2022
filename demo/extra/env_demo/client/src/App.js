bb794c3a111411cccdeb24c5e6dcc801c58d08b8
heidi1105 <heidi1105@gmail.com>
2022-02-02 13:18:37 -0800

    updated environment variable

--- demo/extra/env_demo/client/src/App.js
+++ demo/extra/env_demo/client/src/App.js
@@ -1,5 +1,8 @@
-import react, {useState, useEffect} from "react"
+import React, {useState, useEffect} from "react"
 import axios from "axios"
+import Main from "./components/Main"
+
+
 
 function App() {
   const [secret, setSecret] = useState("")
@@ -10,8 +13,16 @@ function App() {
       .catch()
   },[])
 
+  const msg = process.env.REACT_APP_TESTING_API_KEY
+  const msg2 = process.env.REACT_APP_SECRET_API_KEY
+  const msg3 = process.env.REACT_APP_whatever_message
+
+
   return (
     <div className="App">
+      <h1> process.env.REACT_APP_TESTING_API_KEY:  {msg} </h1>
+      <h1> process.env.REACT_APP_SECRET_API_KEY:  {msg2} </h1>
+      <h1> process.env.REACT_APP_whatever_message:  {msg3}</h1>
       <h1> Secret Message: {secret}</h1>
     </div>
   );
